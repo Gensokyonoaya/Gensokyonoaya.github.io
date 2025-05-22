cd772528bf29d25edbe529be11692d836b54fdec
Gensokyonoaya <3022244161@tju.edu.cn>
2025-05-22 15:30:00 +0800

    Site updated: 2025-05-22 15:30:00

--- js/next-boot.js
+++ js/next-boot.js
@@ -6,6 +6,7 @@ NexT.boot.registerEvents = function() {
 
   NexT.utils.registerScrollPercent();
   NexT.utils.registerCanIUseTag();
+  NexT.utils.updateFooterPosition();
 
   // Mobile top menu bar.
   document.querySelector('.site-nav-toggle .toggle').addEventListener('click', event => {
@@ -26,8 +27,12 @@ NexT.boot.registerEvents = function() {
     const tHash = location.hash;
     if (tHash !== '' && !tHash.match(/%\S{2}/)) {
       const target = document.querySelector(`.tabs ul.nav-tabs li a[href="${tHash}"]`);
-      target && target.click();
+      target?.click();
     }
+  });
+
+  window.addEventListener('tabs:click', e => {
+    NexT.utils.registerCodeblock(e.target);
   });
 };
 
@@ -46,7 +51,7 @@ NexT.boot.refresh = function() {
 
   CONFIG.exturl && NexT.utils.registerExtURL();
   NexT.utils.wrapTableWithBox();
-  NexT.utils.registerCopyCode();
+  NexT.utils.registerCodeblock();
   NexT.utils.registerTabsTag();
   NexT.utils.registerActiveMenuItem();
   NexT.utils.registerLangSelect();
@@ -60,8 +65,8 @@ NexT.boot.motion = function() {
   if (CONFIG.motion.enable) {
     NexT.motion.integrator
       .add(NexT.motion.middleWares.header)
-      .add(NexT.motion.middleWares.postList)
       .add(NexT.motion.middleWares.sidebar)
+      .add(NexT.motion.middleWares.postList)
       .add(NexT.motion.middleWares.footer)
       .bootstrap();
   }
